f7d6f1ef43021debcfad71759a22e15cdd074412
armantekinalp <armant2@illinois.edu>
2023-01-24 23:28:14 -0600

    update:implement 2D fish case parameters

--- examples/3d_examples/FishSwimming/elastic_fish_swimming_case_2d.py
+++ examples/3d_examples/FishSwimming/elastic_fish_swimming_case_2d.py
@@ -2,8 +2,8 @@ import numpy as np
 import sopht.simulator as sps
 import sopht.utils as spu
 import click
-from ..elastic_fish import ElasticFishSimulator
-from ..fish_geometry import create_fish_geometry
+from elastic_fish import ElasticFishSimulator
+from fish_geometry import create_fish_geometry
 
 
 def elastic_fish_swimming_case(
@@ -14,8 +14,10 @@ def elastic_fish_swimming_case(
     mass_ratio: float,
     cauchy_number: float,
     actuation_reynolds_number: float,
-    coupling_stiffness: float = -2e4 / 2,
-    coupling_damping: float = -1e1 / 2,
+    coupling_stiffness: float = -2e4 / 20/2 ,
+    coupling_damping: float = -1e1 / 20/2
+
+        ,
     num_threads: int = 4,
     precision: str = "single",
     save_data: bool = False,
@@ -29,7 +31,7 @@ def elastic_fish_swimming_case(
     y_axis_idx = spu.VectorField.y_axis_idx()
     rho_f = 1
     base_length = 1.0
-    x_range = 6 * base_length
+    x_range = 3 * base_length
     y_range = grid_size_y / grid_size_x * x_range
     # =================PYELASTICA STUFF BEGIN=====================
     period = 1
@@ -58,6 +60,7 @@ def elastic_fish_swimming_case(
         tau_coeff=tau_coeff,
         origin=origin,
         plot_result=True,
+        normal=np.array([0.0, 1.0, 0.0])
     )
     # =================PYELASTICA STUFF END=====================
     # ==================FLOW SETUP START=========================
@@ -70,8 +73,6 @@ def elastic_fish_swimming_case(
         flow_type="navier_stokes_with_forcing",
         real_t=real_t,
         num_threads=num_threads,
-        filter_vorticity=True,
-        filter_setting_dict={"order": 5, "type": "convolution"},
     )
     # ==================FLOW SETUP END=========================
     # ==================FLOW-ROD COMMUNICATOR SETUP START======
@@ -98,7 +99,10 @@ def elastic_fish_swimming_case(
 
     foto_timer = 0.0
     foto_timer_limit = period / 30
-    fish_vel = []
+    time_history = []
+    force_history = []
+    com_velocity_history = []
+    com_position_history = []
 
     # create fig for plotting flow fields
     fig, ax = spu.create_figure_and_axes()
@@ -109,64 +113,70 @@ def elastic_fish_swimming_case(
         if foto_timer >= foto_timer_limit or foto_timer == 0:
             foto_timer = 0.0
             if save_data:
-                ax.set_title(
-                    f"Vorticity, time: {flow_sim.time:.2f}, "
-                    f"distance: {(fish_sim.shearable_rod.position_collection[0, 0] - origin[0]):.6f}"
-                )
-                contourf_obj = ax.contourf(
-                    flow_sim.position_field[x_axis_idx],
-                    flow_sim.position_field[y_axis_idx],
-                    flow_sim.vorticity_field,
-                    levels=np.linspace(-5, 5, 100),
-                    extend="both",
-                    cmap=spu.get_lab_cmap(),
-                )
-                cbar = fig.colorbar(mappable=contourf_obj, ax=ax)
-                element_position = 0.5 * (
-                    fish_sim.shearable_rod.position_collection[:, 1:]
-                    + fish_sim.shearable_rod.position_collection[:, :-1]
-                )
-                # for plotting rod with correct radii for reference see
-                # https://stackoverflow.com/questions/48172928/scale-matplotlib-pyplot
-                # -axes-scatter-markersize-by-x-scale/48174228#48174228
-                scaling_factor = (
-                    ax.get_window_extent().width
-                    / max(flow_sim.x_range, flow_sim.y_range)
-                    * 72.0
-                    / fig.dpi
-                )
-                ax.scatter(
-                    element_position[x_axis_idx],
-                    element_position[y_axis_idx],
-                    s=4 * (scaling_factor * fish_sim.shearable_rod.radius) ** 2,
-                    c="k",
-                )
-                # plot rod and cylinder forcing points
-                # ax.scatter(
-                #     cosserat_rod_flow_interactor.forcing_grid.position_field[x_axis_idx],
-                #     cosserat_rod_flow_interactor.forcing_grid.position_field[y_axis_idx],
-                #     s=5,
-                #     color="g",
-                # )
-                spu.save_and_clear_fig(
-                    fig,
-                    ax,
-                    cbar,
-                    file_name="snap_" + str("%0.4d" % (flow_sim.time * 100)) + ".png",
-                )
+                pass
+            ax.set_title(
+                f"Vorticity, time: {flow_sim.time:.2f}, "
+                f"distance: {(fish_sim.shearable_rod.position_collection[0, 0] - origin[0]):.6f}"
+            )
+            contourf_obj = ax.contourf(
+                flow_sim.position_field[x_axis_idx],
+                flow_sim.position_field[y_axis_idx],
+                flow_sim.vorticity_field,
+                levels=np.linspace(-5, 5, 100),
+                extend="both",
+                cmap=spu.get_lab_cmap(),
+            )
+            cbar = fig.colorbar(mappable=contourf_obj, ax=ax)
+            element_position = 0.5 * (
+                fish_sim.shearable_rod.position_collection[:, 1:]
+                + fish_sim.shearable_rod.position_collection[:, :-1]
+            )
+            # for plotting rod with correct radii for reference see
+            # https://stackoverflow.com/questions/48172928/scale-matplotlib-pyplot
+            # -axes-scatter-markersize-by-x-scale/48174228#48174228
+            scaling_factor = (
+                ax.get_window_extent().width
+                / max(flow_sim.x_range, flow_sim.y_range)
+                * 72.0
+                / fig.dpi
+            )
+            ax.scatter(
+                element_position[x_axis_idx],
+                element_position[y_axis_idx],
+                s=4 * (scaling_factor * fish_sim.shearable_rod.radius) ** 2,
+                c="k",
+            )
+            # plot rod and cylinder forcing points
+            # ax.scatter(
+            #     cosserat_rod_flow_interactor.forcing_grid.position_field[x_axis_idx],
+            #     cosserat_rod_flow_interactor.forcing_grid.position_field[y_axis_idx],
+            #     s=5,
+            #     color="g",
+            # )
+            spu.save_and_clear_fig(
+                fig,
+                ax,
+                cbar,
+                file_name="snap_" + str("%0.4d" % (flow_sim.time * 100)) + ".png",
+            )
             print(
                 f"time: {flow_sim.time:.2f} ({(flow_sim.time/final_time*100):2.1f}%), "
                 f"max_vort: {np.amax(flow_sim.vorticity_field):.4f}, "
                 f"grid deviation L2 error: {cosserat_rod_flow_interactor.get_grid_deviation_error_l2_norm():.6f}, "
                 f"fish pos: {fish_sim.shearable_rod.position_collection[0, 0]:.6f}, "
-                f"flow_dt: {flow_sim.compute_stable_timestep(dt_prefac=0.25):.6f}"
+                f"flow_dt: {flow_sim.compute_stable_timestep(dt_prefac=0.125):.6f}"
             )
 
             # save diagnostics
-            if flow_sim.time >= final_time - 2 * period:
-                fish_vel.append(
-                    fish_sim.shearable_rod.compute_velocity_center_of_mass()[0]
-                )
+            time_history.append(flow_sim.time)
+            forces = np.sum(cosserat_rod_flow_interactor.lag_grid_forcing_field, axis=1)
+            force_history.append(forces.copy())
+            com_velocity_history.append(
+                fish_sim.shearable_rod.compute_velocity_center_of_mass().copy()
+            )
+            com_position_history.append(
+                fish_sim.shearable_rod.compute_position_center_of_mass().copy()
+            )
 
         # compute timestep
         flow_dt = flow_sim.compute_stable_timestep(dt_prefac=0.125)
@@ -181,20 +191,137 @@ def elastic_fish_swimming_case(
         # evaluate feedback/interaction between flow and rod
         cosserat_rod_flow_interactor()
 
+
         # timestep the flow
         flow_sim.time_step(dt=flow_dt)
+
 
         # update timer
         foto_timer += flow_dt
 
     # compile video
     if save_data:
-        spu.make_video_from_image_series(
-            video_name="flow", image_series_name="snap", frame_rate=10
-        )
+        pass
+    spu.make_video_from_image_series(
+        video_name="flow", image_series_name="snap", frame_rate=10
+    )
+    # Save data
+    np.savetxt(
+        "fish_forces_vs_time.csv",
+        np.c_[
+            np.array(time_history),
+            np.array(force_history),
+            np.linalg.norm(np.array(force_history), axis=1),
+        ],
+        delimiter=",",
+        header="time, force x, force y, force z, force norm",
+    )
+    np.savetxt(
+        "fish_velocity_vs_time.csv",
+        np.c_[
+            np.array(time_history),
+            np.array(com_velocity_history),
+            np.linalg.norm(np.array(com_velocity_history), axis=1),
+        ],
+        delimiter=",",
+        header="time, vel x, vel y, vel z, vel norm",
+    )
+    np.savetxt(
+        "fish_com_position_vs_time.csv",
+        np.c_[
+            np.array(time_history),
+            np.array(com_position_history),
+        ],
+        delimiter=",",
+        header="time, pos x, pos y, pos z",
+    )
 
-    print(np.mean(fish_vel))
-    return np.mean(fish_vel)
+    # TODO: remove below lines
+    from scipy.interpolate import CubicSpline
+
+    # Retrieve simulation results
+    time_sim = np.array(fish_sim.rod_post_processing_list[0]["time"])
+    nondim_time = time_sim / period
+
+    # Get non-dimensional position along rod from simulation
+    rest_lengths = np.array(fish_sim.rod_post_processing_list[0]["rest_lengths"][:])
+    s_node = np.zeros((rest_lengths.shape[0], rest_lengths.shape[1]))
+    s_node[:, :] = np.cumsum(rest_lengths, axis=1)
+    s_node /= s_node[:, -1:]
+    s_node_inner = s_node[:, :-1]
+
+    # Get curvatures and positions from simulation
+    curvatures = np.array(fish_sim.rod_post_processing_list[0]["curvature"][:])
+    positions = np.array(fish_sim.rod_post_processing_list[0]["position"][:])
+
+    # Compute error
+    # compare only after ramp up, towards end of sim
+    start: int = np.where(nondim_time >= final_time - 2 * period)[0][0]
+
+    # Compute curvature solution
+    curv_spline = CubicSpline(
+        muscle_torque_coefficients[0, :],
+        muscle_torque_coefficients[1, :],
+        bc_type="natural",
+    )
+    curvatures_amplitude = curv_spline(s_node_inner)
+    curvatures_solution = curvatures_amplitude * np.sin(
+        2.0 * np.pi * (nondim_time[:, np.newaxis] - tau_coeff * s_node_inner)
+    )
+    # curvature error
+    error = np.linalg.norm(
+        curvatures[start:, 1, :] - curvatures_solution[start:, :], axis=1
+    )
+
+    # plot curvature along rod for a few frames to see
+    import matplotlib
+
+    matplotlib.use("Agg")  # Must be before importing matplotlib.pyplot or pylab!
+    from matplotlib import pyplot as plt
+
+    # In Material Frame
+    plt.rcParams.update({"font.size": 22})
+    fig = plt.figure(figsize=(10, 10), frameon=True, dpi=150)
+
+    axs = []
+    axs.append(plt.subplot2grid((1, 1), (0, 0)))
+    skip_frames = 15
+    axs[0].plot(
+        curvatures[start::skip_frames, 1, :].T, "-", color="red", label="simulation"
+    )
+    axs[0].plot(
+        curvatures_solution[start::skip_frames, :].T,
+        "--",
+        color="skyblue",
+        label="solution",
+    )
+    plt.tight_layout()
+    fig.align_ylabels()
+    # fig.legend(prop={"size": 20})
+    fig.savefig("curvature_envelope_comparison.png")
+    plt.close(plt.gcf())
+
+    plt.rcParams.update({"font.size": 22})
+    fig = plt.figure(figsize=(10, 10), frameon=True, dpi=150)
+
+    axs = []
+    axs.append(plt.subplot2grid((1, 1), (0, 0)))
+    axs[0].plot(nondim_time[start:], error, "-", color="red")
+    plt.tight_layout()
+    fig.align_ylabels()
+    fig.savefig("curvature_error.png")
+    plt.close(plt.gcf())
+
+    plt.rcParams.update({"font.size": 22})
+    fig = plt.figure(figsize=(10, 10), frameon=True, dpi=150)
+
+    fig, ax = spu.create_figure_and_axes(fig_aspect_ratio=1.0)
+    ax.plot(
+        positions[start::skip_frames, 0, :].T, positions[start::skip_frames, 2, :].T
+    )
+    plt.tight_layout()
+    fig.savefig("position_envelope.png")
+    plt.close(plt.gcf())
 
 
 if __name__ == "__main__":
@@ -204,11 +331,12 @@ if __name__ == "__main__":
     @click.option("--nx", default=128, help="Number of grid points in x direction.")
     def simulate_fish_swimming(num_threads: int, nx: int) -> None:
         ny = nx // 2
+
         # in order Y, X
         grid_size = (ny, nx)
-        n_elem = nx // 8
+        n_elem = nx // 2
         exp_activation_period = 1.0
-        final_time = 12.0 * exp_activation_period
+        final_time = 6.0 * exp_activation_period
 
         exp_base_length = 1.0
         exp_rho_s = 1e3 / 15  # kg/m3
