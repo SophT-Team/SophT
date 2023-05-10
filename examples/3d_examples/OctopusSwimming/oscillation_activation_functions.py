2bbb9a9b3d9008b96a88e92b66997b5c8318d1d9
armantekinalp <armant2@illinois.edu>
2023-05-10 11:37:33 -0500

    enhancement:swimming octopus case

---
+++ examples/3d_examples/OctopusSwimming/oscillation_activation_functions.py
@@ -0,0 +1,164 @@
+__all__ = ["OscillationActivation"]
+import numpy as np
+
+
+class OscillationActivation:
+    def __init__(
+        self,
+        wave_number,
+        frequency,
+        phase_shift,
+        start_time,
+        end_time,
+        start_non_dim_length,
+        end_non_dim_length,
+        n_elems,
+        a,
+        b,
+        activation_level_max=1.0,
+        activation_level_end=0.0,
+        activation_lower_threshold=2e-14,
+    ):
+
+        self.start_time = start_time
+        self.end_time = end_time
+        self.wave_number = wave_number
+        self.frequency = frequency
+        self.phase_shift = phase_shift
+        self.start_non_dim_length = start_non_dim_length
+        self.end_non_dim_length = end_non_dim_length
+        self.start_idx = int(np.rint(start_non_dim_length * n_elems))
+        self.end_idx = int(np.rint(end_non_dim_length * n_elems))
+        self.activation_level_max = activation_level_max
+        self.activation_lower_threshold = activation_lower_threshold
+        self.a = a
+        self.b = b
+
+        self.non_dimensional_length = np.linspace(0, 1, n_elems)
+
+    def apply_activation(self, system, activation, time: np.float64 = 0.0):
+        n_elems = self.end_idx - self.start_idx
+        index = np.arange(0, n_elems, dtype=np.int64)
+        fiber_activation = np.zeros((n_elems))
+        activation *= 0
+
+        time = round(time, 5)
+
+        if time > self.start_time and time < self.end_time:
+
+            sigmoid = 1 / (1 + np.exp(-self.a * (self.frequency * time - self.b)))
+
+            fiber_activation = (
+                self.activation_level_max
+                * self.non_dimensional_length[::-1]
+                * np.sin(
+                    2 * np.pi * self.wave_number * self.non_dimensional_length
+                    - 2 * np.pi * self.frequency * time / 2
+                    + 2 * np.pi * self.phase_shift
+                )
+                ** 2
+                * sigmoid
+            )
+
+        active_index = np.where(fiber_activation > self.activation_lower_threshold)[0]
+        activation[self.start_idx + active_index] = fiber_activation[active_index]
+
+
+if __name__ == "__main__":
+
+    A = 20
+    omega_r = 10
+    beta = 3
+
+    t1 = A / (18 * omega_r)
+    omega_p = beta * omega_r
+    T_r = 61 * A / (30 * omega_r)
+    T_p = -3 / (5 * beta) * (beta**2 + 60) * t1
+    # For sculling
+    T = T_r + T_p
+    # T = 1
+
+    f_r = 1 / T
+    f_p = 1 / T
+    X_r = 0
+    X_p = 0
+
+    wave_number = 0.05
+
+    n_elem = 50
+    T = 4
+    time = np.linspace(0, 10 * T, 1000)
+    activation = np.zeros((time.shape[0], n_elem))
+    start_non_dim_length = 0
+    end_non_dim_length = 1
+
+    # oscillation_activation = OscillationActivation(
+    #     wave_number=wave_number,
+    #     frequency=f_r,
+    #     phase_shift=X_p + T / 2,
+    #     start_time=0.0,
+    #     end_time=10000,
+    #     start_non_dim_length=start_non_dim_length,
+    #     end_non_dim_length=end_non_dim_length,
+    #     n_elems=n_elem,
+    #     a=40,
+    #     b=0.5,
+    # )
+
+    oscillation_activation = OscillationActivation(
+        wave_number=wave_number,
+        frequency=1 / T,  # f_p,
+        phase_shift=0,  # X_p,
+        start_time=0.0,
+        end_time=10000,
+        start_non_dim_length=start_non_dim_length,
+        end_non_dim_length=end_non_dim_length,
+        n_elems=n_elem,
+        activation_level_max=0.2,
+        a=10,
+        b=0.5,
+    )
+
+    system = None
+
+    for i in range(time.shape[0]):
+        oscillation_activation.apply_activation(system, activation[i, :], time[i])
+
+    non_dimensional_length = oscillation_activation.non_dimensional_length.copy()
+
+    import matplotlib
+
+    matplotlib.use("Agg")  # Must be before importing matplotlib.pyplot or pylab!
+    from matplotlib import pyplot as plt
+
+    # plt.rcParams.update({"font.size": 22})
+    # fig = plt.figure(figsize=(10, 10), frameon=True, dpi=150)
+    #
+    # axs = []
+    # axs.append(plt.subplot2grid((1, 1), (0, 0)))
+    # for i in range(time.shape[0]):
+    #     axs[0].plot(
+    #         non_dimensional_length, activation[i, :], label=f"T={time[i]/T:.2f}"
+    #     )
+    # axs[0].set_xlabel("non dim length", fontsize=20)
+    # axs[0].set_ylabel("activation", fontsize=20)
+    #
+    # plt.tight_layout()
+    # fig.align_ylabels()
+    # fig.legend(prop={"size": 20})
+    # fig.savefig("oscillation_activation_vs_non_dim_pos.png")
+    # plt.close(plt.gcf())
+
+    plt.rcParams.update({"font.size": 22})
+    fig = plt.figure(figsize=(10, 10), frameon=True, dpi=150)
+    axs = []
+    axs.append(plt.subplot2grid((1, 1), (0, 0)))
+    axs[0].plot(time, activation[:, 0])
+    axs[0].set_xlabel("non dim length", fontsize=20)
+    axs[0].set_ylabel("activation", fontsize=20)
+
+    plt.tight_layout()
+    fig.align_ylabels()
+    # fig.legend(prop={"size": 20})
+    fig.savefig("oscillation_activation_vs_time.png")
+    plt.close(plt.gcf())
---
+++ examples/3d_examples/OctopusSwimming/set_environment_tapered_arm.py
@@ -0,0 +1,365 @@
+from collections import defaultdict
+import numpy as np
+import elastica as ea
+from elastica._calculus import _isnan_check
+
+from coomm.actuations.muscles import (
+    force_length_weight_poly,
+)
+from coomm.actuations.muscles import (
+    MuscleGroup,
+    LongitudinalMuscle,
+    ObliqueMuscle,
+    TransverseMuscle,
+    ApplyMuscleGroups,
+)
+
+
+class BaseSimulator(
+    ea.BaseSystemCollection,
+    ea.Constraints,
+    ea.Connections,
+    ea.Forcing,
+    ea.CallBacks,
+    ea.Damping,
+):
+    pass
+
+
+class OctopusArmCallBack(ea.CallBackBaseClass):
+    """
+    Call back function for octopus arm
+    """
+
+    def __init__(self, step_skip: int, callback_params: dict):
+        ea.CallBackBaseClass.__init__(self)
+        self.every = step_skip
+        self.callback_params = callback_params
+
+    def make_callback(self, system, time, current_step: int):
+        if current_step % self.every == 0:
+            self.callback_params["time"].append(time)
+            self.callback_params["step"].append(current_step)
+            self.callback_params["position"].append(system.position_collection.copy())
+            self.callback_params["velocity"].append(system.velocity_collection.copy())
+            self.callback_params["radius"].append(system.radius.copy())
+
+            return
+
+
+class ArmEnvironment:
+    def __init__(
+        self,
+        final_time,
+        time_step=1.0e-5,
+        rendering_fps=30,
+        COLLECT_DATA_FOR_POSTPROCESSING=True,
+    ):
+        # Integrator type
+        self.StatefulStepper = ea.PositionVerlet()
+
+        self.final_time = final_time
+        self.time_step = time_step
+        self.total_steps = int(self.final_time / self.time_step)
+        self.rendering_fps = rendering_fps
+        self.step_skip = int(1.0 / (self.rendering_fps * self.time_step))
+        self.COLLECT_DATA_FOR_POSTPROCESSING = COLLECT_DATA_FOR_POSTPROCESSING
+
+    def get_systems(
+        self,
+    ):
+        return self.rod_list  # [self.shearable_rod]
+
+    def set_arm(self, E, density, rod, rod_id):
+        self.set_rod(E, density, rod)
+        self.set_muscles(rod, rod_id)
+
+    def setup(self, E, density, rod, rod_id):
+        self.set_arm(E, density, rod, rod_id)
+
+    def set_rod(self, E, density, rod):
+        """Set up a rod"""
+
+        self.E = E
+        # self.shearable_rod = rod
+        self.simulator.append(rod)
+
+        """ Set up boundary conditions """
+        self.simulator.constrain(rod).using(
+            ea.GeneralConstraint,
+            # constrained_position_idx=(0,),
+            constrained_director_idx=(0,),
+            # translational_constraint_selector = np.array([True, True, True]),
+            rotational_constraint_selector=np.array([True, True, True]),
+        )
+
+        # Set exponential damper
+        # Below damping tuned for time-step 2.5E-4
+        damp_coefficient = 0.5e-2  # 0.05
+        radius_base = rod.radius[0]
+        damping_constant = (
+            damp_coefficient / density / (np.pi * radius_base**2) / 15 / 1e3 * 100
+        )  # For tapered rod /15 stable
+        self.simulator.dampen(rod).using(
+            ea.AnalyticalLinearDamper,
+            damping_constant=damping_constant,
+            time_step=self.time_step,
+        )
+
+    def set_muscles(self, arm, arm_id):
+        """Add muscle actuation"""
+
+        def add_muscle_actuation(arm):
+            radius_base = 0.012
+            muscle_groups = []
+            LM_ratio_muscle_position = 0.0075 / radius_base
+            OM_ratio_muscle_position = 0.01125 / radius_base
+            AN_ratio_radius = 0.002 / radius_base
+            TM_ratio_radius = 0.0045 / radius_base
+            LM_ratio_radius = 0.003 / radius_base
+            OM_ratio_radius = 0.00075 / radius_base
+            OM_rotation_number = 6
+            shearable_rod_area = np.pi * arm.radius**2
+            TM_rest_muscle_area = shearable_rod_area * (
+                TM_ratio_radius**2 - AN_ratio_radius**2
+            )
+            LM_rest_muscle_area = shearable_rod_area * (LM_ratio_radius**2)
+            OM_rest_muscle_area = shearable_rod_area * (OM_ratio_radius**2)
+            # stress is in unit [Pa]
+            TM_max_muscle_stress = 1.5 * self.E  # 15_000.0
+            LM_max_muscle_stress = 10 * self.E  # 50_000.0 * 2
+            OM_max_muscle_stress = 5 * self.E  # 50_000.0
+            muscle_dict = dict(
+                force_length_weight=force_length_weight_poly,
+            )
+
+            # Add a transverse muscle
+            muscle_groups.append(
+                MuscleGroup(
+                    muscles=[
+                        TransverseMuscle(
+                            rest_muscle_area=TM_rest_muscle_area,
+                            max_muscle_stress=TM_max_muscle_stress,
+                            **muscle_dict,
+                        )
+                    ],
+                    type_name="TM",
+                )
+            )
+
+            # Add 4 longitudinal muscles
+            for k in range(4):
+                muscle_groups.append(
+                    MuscleGroup(
+                        muscles=[
+                            LongitudinalMuscle(
+                                muscle_init_angle=np.pi * 0.5 * k,
+                                ratio_muscle_position=LM_ratio_muscle_position,
+                                rest_muscle_area=LM_rest_muscle_area,
+                                max_muscle_stress=LM_max_muscle_stress,
+                                **muscle_dict,
+                            )
+                        ],
+                        type_name="LM",
+                    )
+                )
+
+            # Add a clockwise oblique muscle group (4 muscles)
+            muscle_groups.append(
+                MuscleGroup(
+                    muscles=[
+                        ObliqueMuscle(
+                            muscle_init_angle=np.pi * 0.5 * m,
+                            ratio_muscle_position=OM_ratio_muscle_position,
+                            rotation_number=OM_rotation_number,
+                            rest_muscle_area=OM_rest_muscle_area,
+                            max_muscle_stress=OM_max_muscle_stress,
+                            **muscle_dict,
+                        )
+                        for m in range(4)
+                    ],
+                    type_name="OM",
+                )
+            )
+
+            # Add a counter-clockwise oblique muscle group (4 muscles)
+            muscle_groups.append(
+                MuscleGroup(
+                    muscles=[
+                        ObliqueMuscle(
+                            muscle_init_angle=np.pi * 0.5 * m,
+                            ratio_muscle_position=OM_ratio_muscle_position,
+                            rotation_number=-OM_rotation_number,
+                            rest_muscle_area=OM_rest_muscle_area,
+                            max_muscle_stress=OM_max_muscle_stress,
+                            **muscle_dict,
+                        )
+                        for m in range(4)
+                    ],
+                    type_name="OM",
+                )
+            )
+
+            for muscle_group in muscle_groups:
+                muscle_group.set_current_length_as_rest_length(arm)
+
+            return muscle_groups
+
+        self.rod_muscle_groups[arm_id] = add_muscle_actuation(arm)
+        # self.muscle_groups = add_muscle_actuation(arm)
+        self.muscle_callback_params_list[arm_id] = [
+            defaultdict(list) for _ in self.rod_muscle_groups[arm_id]
+        ]
+        self.simulator.add_forcing_to(arm).using(
+            ApplyMuscleGroups,
+            muscle_groups=self.rod_muscle_groups[arm_id],  # self.muscle_groups,
+            step_skip=self.step_skip,
+            callback_params_list=self.muscle_callback_params_list[arm_id],
+        )
+
+    def reset(self, E, density, rod_list, arm_rod_list, rigid_body_list):
+        self.simulator = BaseSimulator()
+
+        self.rod_list = rod_list
+        self.arm_rod_list = arm_rod_list
+        self.rod_muscle_groups = []
+        self.muscle_callback_params_list = []
+        self.object_list = rod_list + rigid_body_list
+
+        for i in range(len(rod_list)):
+            self.rod_muscle_groups.append([])
+            self.muscle_callback_params_list.append([])
+
+        for rod_id, rod in enumerate(rod_list):
+            self.setup(E, density, rod, rod_id)
+
+        for body_id, body in enumerate(rigid_body_list):
+            self.simulator.append(body)
+
+        # Callback
+        self.post_processing_dict_list = []
+        for rod_id, rod in enumerate(self.object_list):
+            self.post_processing_dict_list.append(defaultdict(list))
+            self.simulator.collect_diagnostics(rod).using(
+                OctopusArmCallBack,
+                step_skip=self.step_skip,
+                callback_params=self.post_processing_dict_list[rod_id],
+            )
+
+        """ Finalize the simulator and create time stepper """
+
+    def finalize(self):
+        self.simulator.finalize()
+        self.do_step, self.stages_and_updates = ea.extend_stepper_interface(
+            self.StatefulStepper, self.simulator
+        )
+
+        """ Return
+            (1) total time steps for the simulation step iterations
+            (2) systems for controller design
+        """
+        return self.total_steps, self.get_systems()
+
+    def step(self, time, muscle_activations):
+
+        """Set muscle activations"""
+        for rod_id in range(len(self.arm_rod_list)):
+            for muscle_group, activation in zip(
+                self.rod_muscle_groups[rod_id], muscle_activations[rod_id]
+            ):
+                muscle_group.apply_activation(activation)
+
+        """ Run the simulation for one step """
+        time = self.do_step(
+            self.StatefulStepper,
+            self.stages_and_updates,
+            self.simulator,
+            time,
+            self.time_step,
+        )
+
+        """ Done is a boolean to reset the environment before episode is completed """
+        done = False
+        # Position of the rod cannot be NaN, it is not valid, stop the simulation
+        for rod in self.rod_list:
+            invalid_values_condition = _isnan_check(rod.position_collection)
+
+            if invalid_values_condition == True:
+                print("NaN detected in the simulation !!!!!!!!")
+                done = True
+                break
+
+        """ Return
+            (1) current simulation time
+            (2) current systems
+            (3) a flag denotes whether the simulation runs correlectly
+        """
+        return time, self.get_systems(), done
+
+    def save_data(self):
+
+        # Save octopus data
+        n_elem_arm = self.arm_rod_list[0].n_elems
+        n_arms = len(self.arm_rod_list)
+        time = np.array(self.post_processing_dict_list[0]["time"])
+        arm_position_history = np.zeros((n_arms, time.shape[0], 3, n_elem_arm + 1))
+        arm_velocity_history = np.zeros((n_arms, time.shape[0], 3, n_elem_arm + 1))
+        arm_radius_history = np.zeros((n_arms, time.shape[0], n_elem_arm))
+
+        for i in range(n_arms):
+            arm_position_history[i, :, :, :] = np.array(
+                self.post_processing_dict_list[i]["position"]
+            )
+            arm_velocity_history[i, :, :, :] = np.array(
+                self.post_processing_dict_list[i]["velocity"]
+            )
+            arm_radius_history[i, :, :] = np.array(
+                self.post_processing_dict_list[i]["radius"]
+            )
+
+        # Body
+        body_position_history = np.array(
+            self.post_processing_dict_list[n_arms]["position"]
+        )
+        body_velocity_history = np.array(
+            self.post_processing_dict_list[n_arms]["velocity"]
+        )
+        body_radius_history = np.array(self.post_processing_dict_list[n_arms]["radius"])
+
+        # Head
+        head_position_history = np.array(
+            self.post_processing_dict_list[n_arms + 1]["position"]
+        )
+        head_velocity_history = np.array(
+            self.post_processing_dict_list[n_arms + 1]["velocity"]
+        )
+        head_radius_history = np.array(
+            self.post_processing_dict_list[n_arms + 1]["radius"]
+        )
+
+        import os
+
+        current_path = os.getcwd()
+        save_folder = os.path.join(current_path, "data")
+        os.makedirs(save_folder, exist_ok=True)
+        np.savez(
+            os.path.join(save_folder, "octopus_arm_body.npz"),
+            time=time,
+            arm_position_history=arm_position_history,
+            arm_velocity_history=arm_velocity_history,
+            arm_radius_history=arm_radius_history,
+            body_position_history=body_position_history,
+            body_velocity_history=body_velocity_history,
+            body_radius_history=body_radius_history,
+            head_position_history=head_position_history,
+            head_velocity_history=head_velocity_history,
+            head_radius_history=head_radius_history,
+        )
+
+
+class Environment(ArmEnvironment):
+    def get_systems(self):
+        return self.rod_list  # [self.shearable_rod]
+
+    def setup(self, E, density, rod, rod_id):
+        self.set_arm(E, density, rod, rod_id)
