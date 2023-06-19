d757b57ef30292ac108d9048556023b2822b8b04
ilianasiriziba <ilianasiriziba@gmail.com>
2023-06-19 13:22:20 -0500

    initial commit of restrat functionality

---
+++ sopht/utils/restart.py
@@ -0,0 +1,36 @@
+from typing import Type
+import os
+import elastica as ea
+from sopht.simulator import FlowSimulator
+from sopht.utils import IO
+
+
+def restart_simulation(
+    flow_sim: FlowSimulator,
+    restart_example_simulator: Type,
+    io: IO,
+    rod_io: IO,
+    forcing_io: IO,
+    restart_dir: str,
+) -> None:
+    # find latest saved data
+    iter_num = []
+    for filename in os.listdir():
+        if "sopht" in filename and "h5" in filename:
+            iter_num.append(int(filename.split("_")[-1].split(".")[0]))
+
+    if len(iter_num) == 0:
+        print("There is no file to load in the directory")
+        return
+
+    latest = max(iter_num)
+    # load sopht data
+    flow_sim.time = io.load(h5_file_name=f"sopht_{latest:04d}.h5")
+    _ = rod_io.load(h5_file_name=f"rod_{latest:04d}.h5")
+    _ = forcing_io.load(h5_file_name=f"forcing_grid_{latest:04d}.h5")
+    rod_time = ea.load_state(restart_example_simulator, restart_dir, True)
+
+    assert (
+        flow_sim.time == rod_time
+    ), "Simulation time of the flow is not matched with the Elastica, check your inputs!"
+    print(f"sopht_{latest:04d}.h5 has been loaded")
