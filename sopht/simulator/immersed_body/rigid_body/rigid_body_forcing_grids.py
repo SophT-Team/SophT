fix:change the temperature calculation for neumann boundary for cylinders.

sopht/simulator/immersed_body/rigid_body/rigid_body_forcing_grids.py
@@ -223,11 +223,12 @@ class CircularCylinderVirtualLayerTemperatureForcingGrid(CircularCylinderForcing
         self.eul_dx = eul_dx
 
         self.local_frame_relative_position_field += self.eul_dx * self.surface_normals
+        self.compute_lag_grid_position_field()
+        self.compute_lag_grid_velocity_field()
 
     def compute_lag_grid_velocity_field(self) -> None:
         """
-        Cylinder is not heating up or cooling down. Temperature is constant.
-        Thus, velocity field is always constant, and we don't need to compute.
+        Virtual layer does not interact, and it is only used to probe flow data. So here just pass.
 
         Returns
         -------
@@ -243,8 +244,7 @@ class CircularCylinderVirtualLayerTemperatureForcingGrid(CircularCylinderForcing
         lag_grid_forcing_field: np.ndarray,
     ) -> None:
         """
-        Cylinder is not heating up or cooling down.
-        Temperature is constant, so no feedback to the cylinder.
+        Virtual layer does not interact, and it is only used to probe flow data. So here just pass.
 
         Parameters
         ----------
@@ -285,6 +285,8 @@ class CircularCylinderIndirectNeumannConditionForcingGrid(CircularCylinderForcin
         eul_dx: float = 0,
         virtual_layer_interactor: Type = CircularCylinderVirtualLayerTemperatureForcingGrid,
     ) -> None:
+        self.heat_flux_dx = heat_flux * eul_dx
+        self.virtual_layer_interactor = virtual_layer_interactor
         super().__init__(
             grid_dim=grid_dim,
             num_forcing_points=num_forcing_points,
@@ -297,21 +299,19 @@ class CircularCylinderIndirectNeumannConditionForcingGrid(CircularCylinderForcin
         self.compute_lag_grid_position_field()
         self.compute_lag_grid_velocity_field()
 
-        self.heat_flux_dx = heat_flux * eul_dx
-
-        self.virtual_layer_interactor = virtual_layer_interactor
-
     def compute_lag_grid_velocity_field(self) -> None:
         """
-        Cylinder is not heating up or cooling down. Temperature is constant.
-        Thus, velocity field is always constant, and we don't need to compute.
+        Computes temperature on the cylinder surface based on the heat flux and flow temperature.
 
         Returns
         -------
 
         """
 
-        pass
+        self.velocity_field = (
+            -self.heat_flux_dx
+            + self.virtual_layer_interactor.lag_grid_flow_velocity_field
+        )
 
     def transfer_forcing_from_grid_to_body(
         self,
@@ -320,8 +320,7 @@ class CircularCylinderIndirectNeumannConditionForcingGrid(CircularCylinderForcin
         lag_grid_forcing_field: np.ndarray,
     ) -> None:
         """
-        Cylinder is not heating up or cooling down.
-        Temperature is constant, so no feedback to the cylinder.
+        Virtual layer does not interact, and it is only used to probe flow data. So here just pass.
 
         Parameters
         ----------
@@ -334,10 +333,7 @@ class CircularCylinderIndirectNeumannConditionForcingGrid(CircularCylinderForcin
 
         """
 
-        self.velocity_field = (
-            -self.heat_flux_dx
-            + self.virtual_layer_interactor.lag_grid_flow_velocity_field
-        )
+        pass
 
 
 SupportedRigidBody3D = Union[ea.Cylinder, ea.Sphere, RectangularPlane]

tests/test_simulator/test_immersed_body/rigid_body/test_rigid_body_forcing_grids.py
@@ -224,23 +224,24 @@ def test_circular_cylinder_constant_temperature_forcing_grid_validity_of_overrid
 def test_circular_cylinder_virtual_layer_temperature_forcing_grid_initialization(
     num_forcing_points,
 ):
-    cylinder = mock_2d_cylinder()
+    cylinder_correct = mock_2d_cylinder()
+    cylinder_test = mock_2d_cylinder()
     grid_dim = 2
+    eul_dx = 0.04
+    cylinder_correct.radius += eul_dx
     # Here we are assuming that CircularCylinderForcingGrid is tested in previous tests, and we use it to compare
     # the surface grid positions.
     correct_circ_cyl_forcing_grid = sps.CircularCylinderForcingGrid(
         grid_dim=grid_dim,
-        rigid_body=cylinder,
+        rigid_body=cylinder_correct,
         num_forcing_points=num_forcing_points,
     )
-    eul_dx = correct_circ_cyl_forcing_grid.get_maximum_lagrangian_grid_spacing()
     test_circ_cyl_forcing_grid = sps.CircularCylinderVirtualLayerTemperatureForcingGrid(
         grid_dim=grid_dim,
-        rigid_body=cylinder,
+        rigid_body=cylinder_test,
         num_forcing_points=num_forcing_points,
         eul_dx=eul_dx,
     )
-
     np.testing.assert_allclose(
         correct_circ_cyl_forcing_grid.position_field,
         test_circ_cyl_forcing_grid.position_field,
@@ -253,19 +254,8 @@ def test_circular_cylinder_virtual_layer_temperature_forcing_grid_initialization
         atol=get_test_tol(precision="double"),
     )
 
-    # Compute correct surface normals.
-    surface_normals = (
-        correct_circ_cyl_forcing_grid.local_frame_relative_position_field.copy()
-    )
-    surface_normals /= np.linalg.norm(surface_normals, axis=0)
-    # Virtual layer is one dx away from the cylinder.
-    correct_frame_relative_position = (
-        correct_circ_cyl_forcing_grid.local_frame_relative_position_field
-        + eul_dx * surface_normals
-    )
-
     np.testing.assert_allclose(
-        correct_frame_relative_position,
+        correct_circ_cyl_forcing_grid.local_frame_relative_position_field,
         test_circ_cyl_forcing_grid.local_frame_relative_position_field,
         atol=get_test_tol(precision="double"),
     )
@@ -359,9 +349,7 @@ def test_circular_cylinder_indirect_neumann_condition_forcing_grid(num_forcing_p
     correct_temperature_field += -heat_flux * thermal_sim.dx
 
     # Call the method to compute temperature on Neumann forcing grid.
-    test_circ_cyl_forcing_grid.transfer_forcing_from_grid_to_body(
-        np.zeros((1)), np.zeros((1)), np.zeros((1))
-    )
+    test_circ_cyl_forcing_grid.compute_lag_grid_velocity_field()
 
     np.testing.assert_allclose(
         correct_temperature_field,